6cabae1c6e5203415d505994cac9f5a1bceb1955
hrastic2411 <dodimir.hrast@gmail.com>
2022-10-07 09:33:56 +0200

    Normalized email address and added superuser support

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,6 @@
 """
 Database models.
 """
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -15,8 +14,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
