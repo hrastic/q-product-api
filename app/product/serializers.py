426a0cb7896bca9da59d59fa7a3f923e2e030aa7
hrastic2411 <dodimir.hrast@gmail.com>
2022-10-11 11:20:11 +0200

    Added endpoint for storing average rating on the product itself

--- app/product/serializers.py
+++ app/product/serializers.py
@@ -20,10 +20,34 @@ class ProductSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'updated_at']
 
 
+class ProductRatingSerializer(ProductSerializer):
+    """Serializer for product rating."""
+
+    rating = serializers.FloatField(min_value=0, max_value=5)
+
+    class Meta(ProductSerializer.Meta):
+        model = Product
+        read_only_fields = \
+            ProductSerializer.Meta.read_only_fields + \
+            ['name', 'price']
+
+
 class RatingSerializer(serializers.ModelSerializer):
     """Serializer for ratings."""
+
+    rating = serializers.IntegerField(min_value=0, max_value=5)
 
     class Meta:
         model = Rating
         fields = ['id', 'user', 'product', 'rating']
         read_only_fields = ['id']
+
+
+class RatingDetailSerializer(RatingSerializer):
+    """Serializer for ratings detail."""
+
+    class Meta(RatingSerializer.Meta):
+        model = Rating
+        read_only_fields = \
+            RatingSerializer.Meta.read_only_fields + \
+            ['user', 'product']
--- app/product/views.py
+++ app/product/views.py
@@ -3,10 +3,15 @@ Views for the products APIs.
 """
 from rest_framework import (
     viewsets,
-    filters
+    filters,
+    status
 )
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from django.db.models import Avg
 
 from core.models import (
     Product,
@@ -27,6 +32,27 @@ class ProductViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    @action(detail=True, methods=['get'], url_path='store-average-rating')
+    def store_average_rating(self, request, pk=None):
+        """View for storing average rating on the product itself."""
+        product = self.get_object()
+        ratings = Rating.objects.filter(
+            product=product
+        ).aggregate(Avg('rating'))
+        average_rating = round(ratings['rating__avg'], 2)
+        serializer = serializers.ProductRatingSerializer(data={
+            'name': product.name,
+            'rating': average_rating,
+            'price': product.price
+        })
+        if serializer.is_valid():
+            product.rating = serializer.validated_data['rating']
+            product.save()
+            return Response({'status': 'Average rating set'})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     """View for manage rating APIs."""
@@ -36,3 +62,23 @@ class RatingViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.OrderingFilter]
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        """Return only details for ratings by authenticated user."""
+        instance = self.get_object()
+        if instance.user != self.request.user:
+            return Response(
+                {
+                    "non_field_errors":
+                        ["Unable to edit another users' review."]
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return super().update(request, *args, **kwargs)
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'update':
+            return serializers.RatingDetailSerializer
+
+        return self.serializer_class
